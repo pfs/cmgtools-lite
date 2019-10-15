1a15c09bf6a88d29e7a1c7e543e425ff1ae475dc
mdunser <marc.dunser@cern.ch>
2019-10-15 13:36:57 +0200

    postfitplot script now does also inclusive 2l analysis with option --inclusive. also now saves the table as a txt file

--- TTHAnalysis/python/plotter/hin-ttbar/scripts/plotPostFitDistributions.py
+++ TTHAnalysis/python/plotter/hin-ttbar/scripts/plotPostFitDistributions.py
@@ -1,6 +1,10 @@
 import sys, os, optparse
 import ROOT
 
+
+## usage:
+## python plotPostFitDistributions.py /eos/cms/store/cmst3/group/hintt/fits/datacards_2019-09-19_elPt25muPt20_ttbarBRFix/sphericity/allFlavors.card/fitDiagnosticsobs.root --outdir <directoryForPlots> (--inclusive)
+
 mcStack=[('zg'        , 'Z/#gamma^{*}'         , ROOT.kAzure+6) ,
          ('data_comb' , 'nonprompt'            , ROOT.kOrange ) ,
          ('tW'        , 'tW'                   , ROOT.kTeal+9 ) ,
@@ -11,9 +15,16 @@ yranges={'ee0b': [0., 450.], 'mm0b': [0., 1300.], 'em0b': [0., 55.],
          'ee1b': [0.,  45.], 'mm1b': [0.,  170.], 'em1b': [0., 21.],
          'ee2b': [0.,  12.], 'mm2b': [0.,   32.], 'em2b': [0., 12.]}
 
+yrangesInclusive           = {'ee': [0., 300.], 'mm': [0., 1000.], 'em': [0., 40.]}
+yrangesInclusiveSphericity = {'ee': [0., 250.], 'mm': [0.,  500.], 'em': [0., 27.]}
 
 valsAndErrors = {}
 
+
+def saveTable(tex):
+    f = open(options.outdir+'/yieldTable{a}.txt'.format(a='Inclusive' if options.inclusive else ''), 'w')
+    f.write(tex)
+    f.close()
 
 def printTable(vae):
 
@@ -28,25 +39,66 @@ def printTable(vae):
 \\small
 \\topcaption{{
     The number of expected background and signal events and the observed event yields in the different
-    channels of $ee$, $\\mu\\mu$, and $e\\mu$, prior to the fit.
-    \\label{{tab:yieldsJet}}}}
+    channels of $ee$, $\\mu\\mu$, and $e\\mu$, \\textbf{{after the fit}}.
+    \\label{{tab:yieldsJetPostfit}}}}
+    \\resizebox{{\\textwidth}}{{!}}{{
     \\begin{{tabular}}{{lccc|ccc|ccc}}
 Process        & $ee$ 0b    & $ee$ 1b   & $ee$ 2b   & $\\mu\\mu$ 0b   & $\\mu\\mu$ 1b  & $\\mu\\mu$ 2b  & $e\\mu$ 0b    & $e\\mu$  1b    & $e\\mu$  2b   \\\\ \\hline \\hline
 
-Z/\\gamma^{{*}} & {zg_ee0b} \\pm {zg_ee0b_e} & {zg_ee1b} \\pm {zg_ee1b_e} & {zg_ee2b} \\pm {zg_ee2b_e} & {zg_mm0b} \\pm {zg_mm0b_e} & {zg_mm1b} \\pm {zg_mm1b_e} & {zg_mm2b} \\pm {zg_mm2b_e} & {zg_em0b} \\pm {zg_em0b_e} & {zg_em1b} \\pm {zg_em1b_e} & {zg_em2b} \\pm {zg_em2b_e}  \\\\
-Nonprompt & {data_comb_ee0b} \\pm {data_comb_ee0b_e} & {data_comb_ee1b} \\pm {data_comb_ee1b_e} & {data_comb_ee2b} \\pm {data_comb_ee2b_e} & {data_comb_mm0b} \\pm {data_comb_mm0b_e} & {data_comb_mm1b} \\pm {data_comb_mm1b_e} & {data_comb_mm2b} \\pm {data_comb_mm2b_e} & {data_comb_em0b} \\pm {data_comb_em0b_e} & {data_comb_em1b} \\pm {data_comb_em1b_e} & {data_comb_em2b} \\pm {data_comb_em2b_e}  \\\\
-tW & {tW_ee0b} \\pm {tW_ee0b_e} & {tW_ee1b} \\pm {tW_ee1b_e} & {tW_ee2b} \\pm {tW_ee2b_e} & {tW_mm0b} \\pm {tW_mm0b_e} & {tW_mm1b} \\pm {tW_mm1b_e} & {tW_mm2b} \\pm {tW_mm2b_e} & {tW_em0b} \\pm {tW_em0b_e} & {tW_em1b} \\pm {tW_em1b_e} & {tW_em2b} \\pm {tW_em2b_e}  \\\\
-VV & {VV_ee0b} \\pm {VV_ee0b_e} & {VV_ee1b} \\pm {VV_ee1b_e} & {VV_ee2b} \\pm {VV_ee2b_e} & {VV_mm0b} \\pm {VV_mm0b_e} & {VV_mm1b} \\pm {VV_mm1b_e} & {VV_mm2b} \\pm {VV_mm2b_e} & {VV_em0b} \\pm {VV_em0b_e} & {VV_em1b} \\pm {VV_em1b_e} & {VV_em2b} \\pm {VV_em2b_e}  \\\\
-Total background & {total_background_ee0b} \\pm {total_background_ee0b_e} & {total_background_ee1b} \\pm {total_background_ee1b_e} & {total_background_ee2b} \\pm {total_background_ee2b_e} & {total_background_mm0b} \\pm {total_background_mm0b_e} & {total_background_mm1b} \\pm {total_background_mm1b_e} & {total_background_mm2b} \\pm {total_background_mm2b_e} & {total_background_em0b} \\pm {total_background_em0b_e} & {total_background_em1b} \\pm {total_background_em1b_e} & {total_background_em2b} \\pm {total_background_em2b_e}  \\\\
-\\ttbar signal & {ttbar_ee0b} \\pm {ttbar_ee0b_e} & {ttbar_ee1b} \\pm {ttbar_ee1b_e} & {ttbar_ee2b} \\pm {ttbar_ee2b_e} & {ttbar_mm0b} \\pm {ttbar_mm0b_e} & {ttbar_mm1b} \\pm {ttbar_mm1b_e} & {ttbar_mm2b} \\pm {ttbar_mm2b_e} & {ttbar_em0b} \\pm {ttbar_em0b_e} & {ttbar_em1b} \\pm {ttbar_em1b_e} & {ttbar_em2b} \\pm {ttbar_em2b_e}  \\\\
+Z/$\\gamma^{{*}}$ & {zg_ee0b} $\\pm$ {zg_ee0b_e} & {zg_ee1b} $\\pm$ {zg_ee1b_e} & {zg_ee2b} $\\pm$ {zg_ee2b_e} & {zg_mm0b} $\\pm$ {zg_mm0b_e} & {zg_mm1b} $\\pm$ {zg_mm1b_e} & {zg_mm2b} $\\pm$ {zg_mm2b_e} & {zg_em0b} $\\pm$ {zg_em0b_e} & {zg_em1b} $\\pm$ {zg_em1b_e} & {zg_em2b} $\\pm$ {zg_em2b_e}  \\\\
+Nonprompt & {data_comb_ee0b} $\\pm$ {data_comb_ee0b_e} & {data_comb_ee1b} $\\pm$ {data_comb_ee1b_e} & {data_comb_ee2b} $\\pm$ {data_comb_ee2b_e} & {data_comb_mm0b} $\\pm$ {data_comb_mm0b_e} & {data_comb_mm1b} $\\pm$ {data_comb_mm1b_e} & {data_comb_mm2b} $\\pm$ {data_comb_mm2b_e} & {data_comb_em0b} $\\pm$ {data_comb_em0b_e} & {data_comb_em1b} $\\pm$ {data_comb_em1b_e} & {data_comb_em2b} $\\pm$ {data_comb_em2b_e}  \\\\
+tW & {tW_ee0b} $\\pm$ {tW_ee0b_e} & {tW_ee1b} $\\pm$ {tW_ee1b_e} & {tW_ee2b} $\\pm$ {tW_ee2b_e} & {tW_mm0b} $\\pm$ {tW_mm0b_e} & {tW_mm1b} $\\pm$ {tW_mm1b_e} & {tW_mm2b} $\\pm$ {tW_mm2b_e} & {tW_em0b} $\\pm$ {tW_em0b_e} & {tW_em1b} $\\pm$ {tW_em1b_e} & {tW_em2b} $\\pm$ {tW_em2b_e}  \\\\
+VV & {VV_ee0b} $\\pm$ {VV_ee0b_e} & {VV_ee1b} $\\pm$ {VV_ee1b_e} & {VV_ee2b} $\\pm$ {VV_ee2b_e} & {VV_mm0b} $\\pm$ {VV_mm0b_e} & {VV_mm1b} $\\pm$ {VV_mm1b_e} & {VV_mm2b} $\\pm$ {VV_mm2b_e} & {VV_em0b} $\\pm$ {VV_em0b_e} & {VV_em1b} $\\pm$ {VV_em1b_e} & {VV_em2b} $\\pm$ {VV_em2b_e}  \\\\
+Total background & {total_background_ee0b} $\\pm$ {total_background_ee0b_e} & {total_background_ee1b} $\\pm$ {total_background_ee1b_e} & {total_background_ee2b} $\\pm$ {total_background_ee2b_e} & {total_background_mm0b} $\\pm$ {total_background_mm0b_e} & {total_background_mm1b} $\\pm$ {total_background_mm1b_e} & {total_background_mm2b} $\\pm$ {total_background_mm2b_e} & {total_background_em0b} $\\pm$ {total_background_em0b_e} & {total_background_em1b} $\\pm$ {total_background_em1b_e} & {total_background_em2b} $\\pm$ {total_background_em2b_e}  \\\\
+\\ttbar signal & {ttbar_ee0b} $\\pm$ {ttbar_ee0b_e} & {ttbar_ee1b} $\\pm$ {ttbar_ee1b_e} & {ttbar_ee2b} $\\pm$ {ttbar_ee2b_e} & {ttbar_mm0b} $\\pm$ {ttbar_mm0b_e} & {ttbar_mm1b} $\\pm$ {ttbar_mm1b_e} & {ttbar_mm2b} $\\pm$ {ttbar_mm2b_e} & {ttbar_em0b} $\\pm$ {ttbar_em0b_e} & {ttbar_em1b} $\\pm$ {ttbar_em1b_e} & {ttbar_em2b} $\\pm$ {ttbar_em2b_e}  \\\\
 Observed (data) & {obs_ee0b} & {obs_ee1b} & {obs_ee2b} & {obs_mm0b} & {obs_mm1b} & {obs_mm2b} & {obs_em0b} & {obs_em1b} & {obs_em2b}   \\\\
 
-\\end{{tabular}}
+\\end{{tabular}} }}
 
 \\end{{table}}'''.format(**vae)
     print tabletex
 
-def convertGraph(inGraph):
+    saveTable(tabletex)
+
+def printTableInclusive(vae):
+
+    for k,v in vae.items():
+        if not 'obs' in k:
+            vae[k] = '{a:.1f}'.format(a=v)
+        else:
+            vae[k] = '{a:.0f}'.format(a=v)
+
+    tabletex = '''\\begin{table}[!htb]
+    \\centering
+    \\topcaption{ The number of expected background and signal events and the observed event yields in the different
+    channels of $ee$, $\\mu\\mu$, and $e\\mu$, \\textbf{{after the fit}}.
+    \\label{tab:yieldsPostfit}}
+        \\begin{tabular}{lccc}
+            Process        & $ee$  & $\\mu\\mu$ & $e\\mu$ \\\\ \\hline \\hline\n'''
+        
+    tabletex += '''
+    Nonprompt         & {data_comb_ee} $\\pm$  {data_comb_ee_e} & {data_comb_mm} $\\pm$ {data_comb_mm_e} & {data_comb_em} $\\pm$ {data_comb_em_e}       \\\\
+    Z/$\\gamma^{{*}}$ & {zg_ee} $\\pm$  {zg_ee_e} & {zg_mm} $\\pm$  {zg_mm_e} & {zg_em} $\\pm$  {zg_em_e} \\\\
+    $\\cPqt\\PW$      & {tW_ee} $\\pm$  {tW_ee_e} & {tW_mm} $\\pm$  {tW_mm_e} & {tW_em} $\\pm$  {tW_em_e} \\\\
+    VV                & {VV_ee} $\\pm$  {VV_ee_e} & {VV_mm} $\\pm$  {VV_mm_e} & {VV_em} $\\pm$  {VV_em_e} \\\\ \\hline \\hline
+    Total background  & {total_background_ee} $\\pm$ {total_background_ee_e} & {total_background_mm} $\\pm$ {total_background_mm_e} & {total_background_em} $\\pm$ {total_background_em_e}\\\\
+    \\ttbar signal    & {ttbar_ee} $\\pm$  {ttbar_ee_e} & {ttbar_mm} $\\pm$  {ttbar_mm_e} & {ttbar_em} $\\pm$  {ttbar_em_e} \\\\ \\hline \\hline
+    Observed (data)   & {obs_ee} & {obs_mm} & {obs_em} \\\\ \\hline \\hline'''.format(**vae)
+    
+    tabletex += '''
+    
+    \\end{tabular}
+\\end{table}'''
+
+    print tabletex
+
+    saveTable(tabletex)
+
+
+def convertGraph(inGraph, xtitle):
+    if 'BDT' not in xtitle:
+        return inGraph
+
     outGraph = inGraph.Clone(inGraph.GetName()+'_converted')
     for ip in range(inGraph.GetN()):
         tmp_x, tmp_y = ROOT.Double(), ROOT.Double()
@@ -56,11 +108,18 @@ def convertGraph(inGraph):
     
     return outGraph
 
-def convertHisto(inHisto, plotName):
+def convertHisto(inHisto, plotName, xtitle):
+
+    if 'BDT' not in xtitle:
+        return inHisto
+
     plotName = plotName.split('/')[-1]
-    nbins =3 
+    nbins = 3 
     if '2b' in plotName:
         nbins = 1
+
+    if options.inclusive:
+        nbins = 10
 
     outHisto = ROOT.TH1F(inHisto.GetName()+'_converted', '', nbins, 0.5, nbins+0.5)
     for ip in range(inHisto.GetXaxis().GetNbins()+1):
@@ -79,7 +138,8 @@ def doPostFitPlot(url):
 
     inF = ROOT.TFile(url)
     xtitle='BDT' if 'bdtcomb' in url else 'Sphericity'
-    xtitle += ' bin'
+    if 'BDT' in xtitle:
+        xtitle += ' bin'
     for ch in inF.Get('shapes_fit_s').GetListOfKeys():
         chName=ch.GetName()
 
@@ -134,7 +194,7 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
     p1.Draw()
     p1.cd()
     frame=plotsPostfit['total'].Clone('frame')
-    frame=convertHisto(frame,plotName)
+    frame=convertHisto(frame,plotName,xtitle)
     frame.Reset('ICE')
     frame.GetYaxis().SetTitle('Events')
     frame.GetYaxis().SetTitleOffset(0.95)
@@ -143,7 +203,14 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
     frame.GetYaxis().SetLabelSize(0.06)
     frame.GetXaxis().SetTitleSize(0)
     frame.GetXaxis().SetLabelSize(0)
-    frame.GetYaxis().SetRangeUser(yranges[plotName.split('_')[-1]][0], yranges[plotName.split('_')[-1]][1]) 
+
+    myyranges = yranges
+    if options.inclusive and 'BDT' in xtitle:
+        myyranges = yrangesInclusive
+    if options.inclusive and 'hericity' in xtitle:
+        myyranges = yrangesInclusiveSphericity
+
+    frame.GetYaxis().SetRangeUser(myyranges[plotName.split('_')[-1]][0], myyranges[plotName.split('_')[-1]][1]) 
     frame.Draw()
 
     leg = ROOT.TLegend(marginL+0.01,0.70,1,0.90)
@@ -160,7 +227,7 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
     for proc,title,ci in mcStack:
         try:
             plotsPostfit[proc].SetTitle(title)
-            newHisto = convertHisto(plotsPostfit[proc], plotName)
+            newHisto = convertHisto(plotsPostfit[proc], plotName, xtitle)
             newHisto.SetLineColor(1)
             newHisto.SetFillStyle(1001)
             newHisto.SetFillColor(ci)
@@ -197,7 +264,7 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
     totalUnc.SetFillColor(1)
     totalUnc.SetMarkerStyle(1)
     #totalUnc.Draw('e2')
-    totalUncNew = convertGraph(totalUnc)
+    totalUncNew = convertGraph(totalUnc,xtitle)
     totalUncNew.Draw('e2')
 
     plotsPostfit['data'].SetMarkerStyle(20)
@@ -210,7 +277,7 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
         integral += tmp_y
     valsAndErrors['obs_'+channel] = integral
     #plotsPostfit['data'].Draw('PZ')
-    dataGraphNew = convertGraph(plotsPostfit['data'])
+    dataGraphNew = convertGraph(plotsPostfit['data'],xtitle)
     dataGraphNew.Draw('PZ')
 
     leg.Draw('same')
@@ -232,39 +299,6 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
         tex.DrawLatex(0.6,0.65+it*0.05,extraTxt[it])
 
     p1.RedrawAxis()
-
-    ## don't do this #prefit/postfit
-    ## don't do this c.cd()
-
-    ## don't do this p2 = ROOT.TPad("p2", "p1", 0., 0.3, 1., 0.5)
-    ## don't do this p2.SetTopMargin(0.01)
-    ## don't do this p2.SetRightMargin(0.03)
-    ## don't do this p2.SetLeftMargin(0.12)
-    ## don't do this p2.SetBottomMargin(0.01)
-    ## don't do this p2.SetGridy()
-    ## don't do this p2.Draw()
-    ## don't do this p2.cd()
-
-    ## don't do this frame2=frame.Clone()
-    ## don't do this frame2.GetYaxis().SetRangeUser(0.5,1.5)
-    ## don't do this frame2.GetYaxis().CenterTitle()
-    ## don't do this frame2.GetYaxis().SetTitle('Postfit/Prefit')
-    ## don't do this frame2.GetYaxis().SetTitleOffset(0.3)
-    ## don't do this frame2.GetYaxis().SetTitleSize(0.2)
-    ## don't do this frame2.GetYaxis().SetLabelSize(0.2)
-    ## don't do this frame2.Draw()
-
-    ## don't do this leg2 = ROOT.TLegend(0.15,0.77,0.65,0.93)
-    ## don't do this leg2.SetNColumns(len(ratios))
-    ## don't do this leg2.SetFillStyle(0)
-    ## don't do this leg2.SetBorderSize(0)
-    ## don't do this leg2.SetTextSize(0.2)
-    ## don't do this for r in ratios:
-    ## don't do this     r.Draw('histsame')
-    ## don't do this     leg2.AddEntry(r,r.GetTitle(),'l')
-    ## don't do this leg2.Draw()
-
-    ## don't do this p2.RedrawAxis()
 
     #data/MC
     c.cd()
@@ -311,7 +345,7 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
     #relPreUncGr.Draw('e2')
     relFitUncGr=getRelUnc(plotsPostfit, 'total', ROOT.kAzure-3, 3001)
     #relFitUncGr.Draw('e2')
-    relFitUncGr2 = convertGraph(relFitUncGr)
+    relFitUncGr2 = convertGraph(relFitUncGr, xtitle)
     relFitUncGr2.Draw('e2')
     #print 'this is relFitUncGr', relFitUncGr
     #relFitUncGr.GetYaxis().SetRangeUser(0., 2.)
@@ -333,7 +367,7 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
             data2fitGr.SetPointEYlow(i,eylo/den)
 
     #data2fitGr.Draw('PZ')
-    data2fitGrNew = convertGraph(data2fitGr)
+    data2fitGrNew = convertGraph(data2fitGr, xtitle)
     data2fitGrNew.Draw('PZ')
 
     leg3 = ROOT.TLegend(0.15,0.85,0.8,0.95)
@@ -361,8 +395,8 @@ def compareFitResult(plotsPrefit,plotsPostfit,plotName,xtitle,extraTxt=[]):
 
 if __name__ == "__main__":
     parser = optparse.OptionParser(usage='usage: %prog [opts] ', version='%prog 1.0')
-    parser.add_option('--outdir', type='string'       , default=''    , help='output directory where the postfit plots are saved.')
-    #parser.add_option('--simple'    ,                  dest='simple'       , action='store_true' , default=False , help='make simple plot')
+    parser.add_option('--outdir'   , type='string'       , default=''    , help='output directory where the postfit plots are saved.')
+    parser.add_option('--inclusive', action='store_true' , default=False , help='do it for inclusive plots (i.e. non b-tagged)')
     (options, args) = parser.parse_args()
 
     url=sys.argv[1]
@@ -377,6 +411,9 @@ if __name__ == "__main__":
 
     doPostFitPlot(url)
 
-    printTable(valsAndErrors)
+    if options.inclusive:
+        printTableInclusive(valsAndErrors)
+    else:
+        printTable(valsAndErrors)
 
     #sys.exit()
